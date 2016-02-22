7092e634758a7c6452969d7435906b3afc8f6582
Avishay Bassa <avishay@geekmedia.co.il>
2016-02-22 11:19:03 +0200

    Updated iOS example.

--- examples/AwesomeProject/index.ios.js
+++ examples/AwesomeProject/index.ios.js
@@ -11,6 +11,37 @@ import React, {
   View
 } from 'react-native';
 
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
+
+// var pendingNotifications = []; if we're pending for an object to get initialized.
+
+// function handleNotification (notification) { // If you want to handle the notifiaction with a payload.
+    // _navigator.to('main.post', notification.data.title, {
+    //  article: {
+    //    title: notification.data.title,
+    //    link: notification.data.url,
+    //    action: notification.data.actionSelected
+    //  }
+    // });
+// }
+
+OneSignal.configure({
+    onNotificationOpened: function(message, data, isActive) {
+        console.log('MESSAGE: ', message);
+        console.log('DATA: ', data);
+        console.log('ISACTIVE: ', isActive);
+
+        // var notification = {message: message, data: data, isActive: isActive};
+        // console.log('NOTIFICATION OPENED: ', notification);
+        //if (!_navigator) { // If we want to wait for an object to get initialized
+        //    console.log('Navigator is null, adding notification to pending list...');
+            // pendingNotifications.push(notification);
+        //    return;
+        // }
+        // handleNotification(notification);
+    }
+});
+
 class AwesomeProject extends Component {
   render() {
     return (
