e8359ddc1e34ccf90aac4a049d50a607757371c6
Avishay Bassa <avishay@geekmedia.co.il>
2017-02-03 01:48:06 +0200

    - Changed to class structure. - Calling events via listeners, using NativeAppEventEmitter instead of DeviceEventEmitter.

--- examples/RNOneSignal/index.android.js
+++ examples/RNOneSignal/index.android.js
@@ -6,66 +6,84 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
 import OneSignal from 'react-native-onesignal';
 
-OneSignal.configure({
-  onIdsAvailable: function (device) {
-    console.log('UserId = ', device.userId);
-    console.log('PushToken = ', device.pushToken);
-  },
-  onNotificationReceived: function (notification) {
-    console.log('NOTIFICATION RECEIVED: ', notification);
-  },
-  onNotificationOpened: function (openResult) {
-    console.log('NOTIFICATION OPENED: ', openResult);
-    console.log('MESSAGE: ', openResult.notification.payload.body);
-    console.log('DATA: ', openResult.notification.payload.additionalData);
-    console.log('ISACTIVE: ', openResult.notification.isAppInFocus);
-  }
-});
-
 export default class RNOneSignal extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    
+    componentWillMount() {
+        OneSignal.addEventListener('received', this.onReceived);
+        OneSignal.addEventListener('opened', this.onOpened);
+        OneSignal.addEventListener('registered', this.onRegistered);
+        OneSignal.addEventListener('ids', this.onIds);
+    }
+
+    componentWillUnmount() {
+        OneSignal.removeEventListener('received', this.onReceived);
+        OneSignal.removeEventListener('opened', this.onOpened);
+        OneSignal.removeEventListener('registered', this.onRegistered);
+        OneSignal.removeEventListener('ids', this.onIds);
+    }
+
+    onReceived(notification) {
+        console.log("Notification received: ", notification);
+    }
+
+    onOpened(openResult) {
+      console.log('Message: ', openResult.notification.payload.body);
+      console.log('Data: ', openResult.notification.payload.additionalData);
+      console.log('isActive: ', openResult.notification.isAppInFocus);
+      console.log('openResult: ', openResult);
+    }
+
+    onRegistered(notifData) {
+        console.log("Device had been registered for push notifications!", notifData);
+    }
+
+    onIds(device) {
+		console.log('Device info: ', device);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('RNOneSignal', () => RNOneSignal);
