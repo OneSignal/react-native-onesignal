6e0bf1bc265efd5eb3ea012c95479b9bf56b7136
Steffen Forkmann <sforkmann@gmail.com>
2017-07-28 14:22:46 +0200

    Remove deprecated method for RN 0.47 compat

--- android/src/main/java/com/geektime/rnonesignalandroid/ReactNativeOneSignalPackage.java
+++ android/src/main/java/com/geektime/rnonesignalandroid/ReactNativeOneSignalPackage.java
@@ -27,11 +27,6 @@ public class ReactNativeOneSignalPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return new ArrayList<>();
     }
