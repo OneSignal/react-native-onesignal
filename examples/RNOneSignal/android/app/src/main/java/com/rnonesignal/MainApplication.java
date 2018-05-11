31ac0627ab7b6aa2f251933cd9409bd7c4f30dad
Brad Hesse <nightsd01@gmail.com>
2018-05-10 17:54:46 -0700

    Improves RN Demo
    • Unifies the Android and iOS view source files into a single file & entry point
    • Updates react dependencies to the most recent versions
    • Adds Privacy consent to the demo (turned off by default)
    • Adds a Prompt Location button

--- examples/RNOneSignal/android/app/src/main/java/com/rnonesignal/MainApplication.java
+++ examples/RNOneSignal/android/app/src/main/java/com/rnonesignal/MainApplication.java
@@ -29,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactNativeOneSignalPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
