30e4e9a0994a57a4035a22dc53192c04580931e2
Adam Skinner <adam.skinner@gmail.com>
2018-05-15 14:21:13 -0400

    Update RCTOneSignal.h
    I stumbled across this while integrating react-native-app-auth.  `OneSignal.h` does not exist in the same directory as `RCTOneSignal.h`, but rather in its parent directory.  I was unable to resolve using the normal `<>` method (which trolls header paths to find the files).

--- ios/RCTOneSignal/RCTOneSignal.h
+++ ios/RCTOneSignal/RCTOneSignal.h
@@ -2,7 +2,7 @@
 #if __has_include(<OneSignal/OneSignal.h>)
 #import <OneSignal/OneSignal.h>
 #else
-#import "OneSignal.h"
+#import "../OneSignal.h"
 #endif
 
 #define INIT_DEPRECATION_NOTICE "Objective-C Initialization of the OneSignal SDK has been deprecated. Use JavaScript init instead."
