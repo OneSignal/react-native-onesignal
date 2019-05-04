2d26499e586bf2b5f7d38099805e68618c16b2e2
Rodrigo <rogomezpalacio@protonmail.com>
2019-05-03 17:51:57 -0700

    New example app from scratch

--- examples/RNOneSignal/android/app/src/main/java/com/rnonesignal/MainApplication.java
+++ examples/RNOneSignal/android/app/src/main/java/com/rnonesignal/MainApplication.java
@@ -1,11 +1,8 @@
 package com.rnonesignal;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactNativeOneSignalPackage()
+          new MainReactPackage()
       );
     }
 
