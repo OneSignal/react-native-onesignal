35e228a75131b56aa92047769f535180516a3ebc
Rodrigo <rogomezpalacio@protonmail.com>
2019-05-09 16:10:47 -0700

    Completed build for iOS

---
+++ examples/RNOneSignal/ios/OneSignalNotificationServiceExtension/NotificationService.m
@@ -0,0 +1,40 @@
+#import <RCTOneSignalExtensionService.h>
+
+#import "NotificationService.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNNotificationRequest *receivedRequest;
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+  self.receivedRequest = request;
+  self.contentHandler = contentHandler;
+  self.bestAttemptContent = [request.content mutableCopy];
+  
+  [RCTOneSignalExtensionService didReceiveNotificationRequest:self.receivedRequest withContent:self.bestAttemptContent];
+  
+  // DEBUGGING: Uncomment the 2 lines below and comment out the one above to ensure this extension is excuting
+  //            Note, this extension only runs when mutable-content is set
+  //            Setting an attachment or action buttons automatically adds this
+  // NSLog(@"Running NotificationServiceExtension");
+  // self.bestAttemptContent.body = [@"[Modified] " stringByAppendingString:self.bestAttemptContent.body];
+  
+  self.contentHandler(self.bestAttemptContent);
+}
+
+- (void)serviceExtensionTimeWillExpire {
+  // Called just before the extension will be terminated by the system.
+  // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+  
+  [RCTOneSignalExtensionService serviceExtensionTimeWillExpireRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent];
+  
+  self.contentHandler(self.bestAttemptContent);
+}
+
+@end
