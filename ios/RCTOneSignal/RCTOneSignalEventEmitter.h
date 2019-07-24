f6c252aa704dbadcb039905c16b06289a13af6ca
Amadeu <amadeu01@gmail.com>
2019-07-24 17:41:11 -0300

    Add InAppMessage event to iOS

--- ios/RCTOneSignal/RCTOneSignalEventEmitter.h
+++ ios/RCTOneSignal/RCTOneSignalEventEmitter.h
@@ -16,12 +16,12 @@ typedef NS_ENUM(NSInteger, OSNotificationEventTypes) {
     NotificationReceived,
     NotificationOpened,
     IdsAvailable,
-    EmailSubscriptionChanged
+    EmailSubscriptionChanged,
+    InAppMessageClicked
 };
 
-#define OSNotificationEventTypesArray @[@"OneSignal-remoteNotificationReceived",@"OneSignal-remoteNotificationOpened",@"OneSignal-idsAvailable",@"OneSignal-emailSubscription"]
+#define OSNotificationEventTypesArray @[@"OneSignal-remoteNotificationReceived",@"OneSignal-remoteNotificationOpened",@"OneSignal-idsAvailable",@"OneSignal-emailSubscription",@"OneSignal-inAppMessageClicked"]
 #define OSEventString(enum) [OSNotificationEventTypesArray objectAtIndex:enum]
-
 
 @interface RCTOneSignalEventEmitter : RCTEventEmitter <RCTBridgeModule>
 
