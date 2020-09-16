49ad07e45735d60c54cc66b95a0af2e38a20e36f
Rodrigo <rogomezpalacio@protonmail.com>
2020-09-15 22:19:58 -0500

    File with event string constants

---
+++ src/events.js
@@ -0,0 +1,7 @@
+// events
+export const NOTIFICATION_WILL_SHOW = 'OneSignal-notificationWillShowInForeground';
+export const NOTIFICATION_OPENED    = 'OneSignal-remoteNotificationOpened'
+export const IN_APP_MESSAGE_CLICKED = 'OneSignal-inAppMessageClicked';
+export const PERMISSION_CHANGED = 'OneSignal-permissionChanged';
+export const SUBSCRIPTION_CHANGED = 'OneSignal-subscriptionChanged';
+export const EMAIL_SUBSCRIPTION_CHANGED = 'OneSignal-emailSubscriptionChanged';
