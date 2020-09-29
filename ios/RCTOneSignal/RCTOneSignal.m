937d4ddfeff54106f920863bcf2ff49aec632073
Rodrigo <rogomezpalacio@protonmail.com>
2020-09-29 13:53:47 -0500

    iOS - RCTOneSignal implementation

--- ios/RCTOneSignal/RCTOneSignal.m
+++ ios/RCTOneSignal/RCTOneSignal.m
@@ -42,38 +42,58 @@ OSNotificationOpenedResult* coldStartOSNotificationOpenedResult;
 }
 
 - (void)initOneSignal {
-    [OneSignal setValue:@"react" forKey:@"mSDKType"];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didBeginObserving) name:@"didSetBridge" object:nil];
-    
-    [OneSignal initWithLaunchOptions:nil appId:nil handleNotificationReceived:^(OSNotification* notification) {
-        [self handleRemoteNotificationReceived:[notification stringify]];
-    } handleNotificationAction:^(OSNotificationOpenedResult *result) {
-        if (!RCTOneSignal.sharedInstance.didStartObserving)
-            coldStartOSNotificationOpenedResult = result;
-        else
-            [self handleRemoteNotificationOpened:[result stringify]];
-        
-    } settings:@{@"kOSSettingsKeyInOmitNoAppIdLogging" : @true, kOSSettingsKeyAutoPrompt : @false}]; //default autoPrompt to false since init will be called again
-    didInitialize = false;
+
+    if (didInitialize)
+        return;
+
+    didInitialize = true;
+    [OneSignal addSubscriptionObserver:self];
+    [OneSignal addEmailSubscriptionObserver:self];
+    [OneSignal initWithLaunchOptions:nil];
 }
 
-// deprecated init methods
-// provides backwards compatibility
-- (id)initWithLaunchOptions:(NSDictionary *)launchOptions appId:(NSString *)appId {
-    return [self initWithLaunchOptions:launchOptions appId:appId settings:nil];
+- (void)handleRemoteNotificationOpened:(NSString *)result {
+    NSDictionary *json = [self jsonObjectWithString:result];
+
+    if (json)
+        [self sendEvent:OSEventString(NotificationOpened) withBody:json];
 }
 
-- (id)initWithLaunchOptions:(NSDictionary *)launchOptions appId:(NSString *)appId settings:(NSDictionary*)settings {
-    [self configureWithAppId:appId settings:settings];
-    
-    return self;
+- (NSDictionary *)jsonObjectWithString:(NSString *)jsonString {
+    NSError *jsonError;
+    NSData *data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&jsonError];
+
+    if (jsonError) {
+        [OneSignal onesignal_Log:ONE_S_LL_ERROR message:[NSString stringWithFormat:@"Unable to serialize JSON string into an object: %@", jsonError]];
+        return nil;
+    }
+
+    return json;
+}
+
+- (void)sendEvent:(NSString *)eventName withBody:(NSDictionary *)body {
+    [RCTOneSignalEventEmitter sendEventWithName:eventName withBody:body];
+}
+
+- (void)onOSSubscriptionChanged:(OSSubscriptionStateChanges * _Nonnull)stateChanges {
+    [self sendEvent:OSEventString(SubscriptionChanged) withBody:stateChanges.to.toDictionary];
+}
+
+- (void)onOSEmailSubscriptionChanged:(OSEmailSubscriptionStateChanges * _Nonnull)stateChanges {
+    // Example of detecting subscribing to OneSignal
+    if (!stateChanges.from.subscribed && stateChanges.to.subscribed) {
+        //Subscribed for OneSignal push notifications!
+    }
+
+    [self sendEvent:OSEventString(EmailSubscriptionChanged) withBody:stateChanges.to.toDictionary];
 }
 
 - (void)didBeginObserving {
     // To continue supporting deprecated initialization methods (which create a new RCTOneSignal instance),
     // we will only access the didStartObserving property of the shared instance to avoid issues
     RCTOneSignal.sharedInstance.didStartObserving = true;
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
         if (coldStartOSNotificationOpenedResult) {
             [self handleRemoteNotificationOpened:[coldStartOSNotificationOpenedResult stringify]];
@@ -84,78 +104,6 @@ OSNotificationOpenedResult* coldStartOSNotificationOpenedResult;
 
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
-- (void)configureWithAppId:(NSString *)appId {
-    [self configureWithAppId:appId settings:nil];
-}
-
-- (void)configureWithAppId:(NSString *)appId settings:(NSDictionary*)settings {
-    
-    if (didInitialize)
-        return;
-    
-    didInitialize = true;
-    [OneSignal addSubscriptionObserver:self];
-    [OneSignal addEmailSubscriptionObserver:self];
-    [OneSignal initWithLaunchOptions:nil
-                               appId:appId
-          handleNotificationReceived:^(OSNotification* notification) {
-              [self handleRemoteNotificationReceived:[notification stringify]];
-          }
-          handleNotificationAction:^(OSNotificationOpenedResult *result) {
-              if (!RCTOneSignal.sharedInstance.didStartObserving)
-                  coldStartOSNotificationOpenedResult = result;
-              else
-                  [self handleRemoteNotificationOpened:[result stringify]];
-              
-          }
-          settings:settings];
-}
-
--(void)onOSEmailSubscriptionChanged:(OSEmailSubscriptionStateChanges *)stateChanges {
-    [self sendEvent:OSEventString(EmailSubscriptionChanged) withBody:stateChanges.to.toDictionary];
-}
-
-- (void)onOSSubscriptionChanged:(OSSubscriptionStateChanges*)stateChanges {
-    
-    // Example of detecting subscribing to OneSignal
-    if (!stateChanges.from.subscribed && stateChanges.to.subscribed) {
-        //Subscribed for OneSignal push notifications!
-    }
-    
-    [self sendEvent:OSEventString(IdsAvailable) withBody:stateChanges.to.toDictionary];
-}
-
-- (void)handleRemoteNotificationReceived:(NSString *)notification {
-    NSDictionary *json = [self jsonObjectWithString:notification];
-    
-    if (json)
-        [self sendEvent:OSEventString(NotificationReceived) withBody:json];
-}
-
-- (void)handleRemoteNotificationOpened:(NSString *)result {
-    NSDictionary *json = [self jsonObjectWithString:result];
-    
-    if (json)
-        [self sendEvent:OSEventString(NotificationOpened) withBody:json];
-}
-
-- (NSDictionary *)jsonObjectWithString:(NSString *)jsonString {
-    NSError *jsonError;
-    NSData *data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
-    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&jsonError];
-    
-    if (jsonError) {
-        [OneSignal onesignal_Log:ONE_S_LL_ERROR message:[NSString stringWithFormat:@"Unable to serialize JSON string into an object: %@", jsonError]];
-        return nil;
-    }
-    
-    return json;
-}
-
-- (void)sendEvent:(NSString *)eventName withBody:(NSDictionary *)body {
-    [RCTOneSignalEventEmitter sendEventWithName:eventName withBody:body];
 }
 
 @end
