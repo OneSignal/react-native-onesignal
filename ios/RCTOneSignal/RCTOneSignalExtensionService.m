d167e1308b04c518e011a8e6a9e84888c2eed7ec
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-07-13 20:32:18 -0300

    Release commit 4.1.0
    * Update Android version to 4.4.1
    * Update iOS Version to 3.5.3
    * Update Notification Service method

--- ios/RCTOneSignal/RCTOneSignalExtensionService.m
+++ ios/RCTOneSignal/RCTOneSignalExtensionService.m
@@ -13,6 +13,10 @@
     [OneSignal didReceiveNotificationExtensionRequest:request withMutableNotificationContent:content];
 }
 
++ (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContent:(UNMutableNotificationContent* _Nullable)content withContentHandler:(void (^)(UNNotificationContent *_Nonnull))contentHandler {
+    [OneSignal didReceiveNotificationExtensionRequest:request withMutableNotificationContent:content withContentHandler:contentHandler];
+}
+
 +(void)serviceExtensionTimeWillExpireRequest:(UNNotificationRequest *)request withMutableNotificationContent:(UNMutableNotificationContent * _Nullable)content {
     [OneSignal serviceExtensionTimeWillExpireRequest:request withMutableNotificationContent:content];
 }
