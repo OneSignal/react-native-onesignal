65fba3f8e95394f96d3be91420df9ac0d40cf095
Nan <nanftw@gmail.com>
2021-10-25 17:25:28 -0700

    Moves types from `index.d.ts` to own files
    Make dir /models
    - Subscription.ts
    - InAppMessage.ts
    - NotificationEvents.ts
    - Outcomes.ts

---
+++ src/models/NotificationEvents.ts
@@ -0,0 +1,13 @@
+import OSNotification from "../OSNotification";
+
+// 0 = NotificationClicked, 1 = ButtonClicked
+export type OpenedEventActionType = 0 | 1;
+
+export interface OpenedEvent {
+    action          : OpenedEventAction;
+    notification    : OSNotification;
+}
+
+export interface OpenedEventAction {
+    type : OpenedEventActionType
+}
