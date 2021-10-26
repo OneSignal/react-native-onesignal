a74947e4d92194e84ec9c0a21fba24007c1aac48
Nan <nanftw@gmail.com>
2021-10-25 17:25:28 -0700

    Moves types from `index.d.ts` to own files
    Make dir /models
    - Subscription.ts
    - InAppMessage.ts
    - NotificationEvents.ts
    - Outcomes.ts

---
+++ src/models/InAppMessage.ts
@@ -0,0 +1,19 @@
+export interface InAppMessage {
+    messageId : string
+}
+
+export interface InAppMessageAction {
+    closes_message  : boolean;
+    first_click     : boolean;
+    click_name      ?: string;
+    click_url       ?: string;
+    outcomes        ?: object[];
+    tags            ?: object;
+}
+
+export interface InAppMessageLifecycleHandlerObject {
+    onWillDisplayInAppMessage       ?: (message: InAppMessage) => void;
+    onDidDisplayInAppMessage        ?: (message: InAppMessage) => void;
+    onWillDismissInAppMessage       ?: (message: InAppMessage) => void;
+    onDidDismissInAppMessage        ?: (message: InAppMessage) => void;
+}
---
+++ src/models/Outcomes.ts
@@ -0,0 +1,7 @@
+export interface OutcomeEvent {
+    session         : string;
+    id              : string;
+    timestamp       : number;
+    weight          : number;
+    notification_ids: string[];
+}
