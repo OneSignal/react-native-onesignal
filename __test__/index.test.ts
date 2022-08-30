0e7be2a1e5852424b1af1ce4b2cad8971b2bc832
fhboswell <franklin.henry.boswell@gmail.com>
2022-08-30 12:03:32 -0700

    [Qualityathon]-test

---
+++ __test__/index.test.ts
@@ -0,0 +1,6 @@
+
+describe("Test for initial Jest setup.", () => {
+    test('two plus two is four', () => {
+        expect(2 + 2).toBe(4);
+      });
+});
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    preset: "react-native",
+    moduleDirectories: ["node_modules","src"],
+    modulePathIgnorePatterns: ["examples", "dist"],
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*"],
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ]
+}
