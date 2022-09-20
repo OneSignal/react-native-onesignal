ff128771e10a6a85a7b3fddcc58abecbfe9f7a34
Jordan Adler <jordan.m.adler@gmail.com>
2022-09-20 14:02:23 -0700

    Add __test__ to prettier

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,7 +1,5 @@
-
-describe("Test for initial jest setup.", () => {
-    test('two plus two is four', () => {
-        expect(2 + 2).toBe(4);
-      });
+describe('Test for initial jest setup.', () => {
+  test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
 });
-
