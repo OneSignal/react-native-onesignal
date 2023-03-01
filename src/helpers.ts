560f53fc093f0e6ebf81a9268a1ebb23bd6ae390
ruslankesheshyan <ruslan@onesignal.com>
2023-02-28 17:51:12 -0800

    - React Native API

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,11 +13,11 @@ export function isValidCallback(handler: Function) {
 
 export function isNativeModuleLoaded(module: NativeModule): boolean {
   if (module == null) {
-    console.error(
-      'Could not load RNOneSignal native module. Make sure native dependencies are properly linked.',
-    );
+    console.error('Could not load RNOneSignal native module. Make sure native dependencies are properly linked.');
+    
     return false;
   }
+
   return true;
 }
 
