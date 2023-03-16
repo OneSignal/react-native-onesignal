54b4d6bc8b8744cab29a638834a27b7a156f0a2a
ruslankesheshyan <ruslan@onesignal.com>
2023-03-16 10:40:40 -0700

    - Example update

--- examples/RNOneSignalTS/src/Helpers.tsx
+++ examples/RNOneSignalTS/src/Helpers.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { Button, View, StyleSheet, Platform, KeyboardAvoidingView, TextInput } from "react-native";
+import {View, StyleSheet, Platform, KeyboardAvoidingView, TextInput} from "react-native";
+import {Button} from "@react-native-material/core";
+
 const disabledColor = '#BEBEBE';
 
-export const renderButtonView = (name: string, color: string, callback: Function) => {
+export const renderButtonView = (name: string, callback: Function) => {
     return (
         <View
             key={name + '_parent'}
@@ -11,21 +13,19 @@ export const renderButtonView = (name: string, color: string, callback: Function
             <Button
                 key={name}
                 title={name}
-                color={color}
                 onPress={() => { callback() }}
             />
         </View>
     );
 }
 
-export const renderFieldView = (name: string, value: string, callback: Function) => {
+export const renderFieldView = (name: string, value: string, callback: (text: string) => void) => {
     return (
         <KeyboardAvoidingView
             key={name + '_keyboard_avoiding_view'}
             style={{
                 width: 300,
                 height: 40,
-                borderColor: textInputBorderColor,
                 borderWidth: 2,
                 borderRadius: 5,
                 marginTop: 8}}
@@ -48,7 +48,6 @@ export const renderFieldView = (name: string, value: string, callback: Function)
     );
 }
 
-/* S T Y L E S */
 const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'column',
