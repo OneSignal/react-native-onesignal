24bea92d26174a00c178edd75add2842c8eb672d
Nan <nanftw@gmail.com>
2023-10-06 12:55:01 -0700

    release commit

--- android/src/main/java/com/onesignal/rnonesignalandroid/RNOneSignal.java
+++ android/src/main/java/com/onesignal/rnonesignalandroid/RNOneSignal.java
@@ -216,7 +216,7 @@ public class RNOneSignal extends ReactContextBaseJavaModule implements
     public void initialize(String appId) {
         Context context = mReactApplicationContext.getCurrentActivity();
         OneSignalWrapper.setSdkType("reactnative");
-        OneSignalWrapper.setSdkVersion("050001");
+        OneSignalWrapper.setSdkVersion("050002");
 
         if (oneSignalInitDone) {
             Log.e("OneSignal", "Already initialized the OneSignal React-Native SDK");
