0421f6171bed9e28d9d5103f83c496adffd701fe
Nan <nanftw@gmail.com>
2023-10-29 20:28:58 -0700

    docs: fix optIn and optOut description

--- src/index.ts
+++ src/index.ts
@@ -249,14 +249,14 @@ export namespace OneSignal {
         return pushSub.optedIn;
       }
 
-      /** Enable the push notification subscription to OneSignal. */
+      /** Disable the push notification subscription to OneSignal. */
       export function optOut() {
         if (!isNativeModuleLoaded(RNOneSignal)) return;
 
         RNOneSignal.optOut();
       }
 
-      /** Disable the push notification subscription to OneSignal. */
+      /** Enable the push notification subscription to OneSignal. */
       export function optIn() {
         if (!isNativeModuleLoaded(RNOneSignal)) return;
 
