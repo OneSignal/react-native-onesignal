3efafdf22f1d87835fbf5d1d56d69436ec2291f2
Jenna Antilla <46546946+jennantilla@users.noreply.github.com>
2024-01-09 16:12:08 -0800

    Update User namespace
    - add getters for onesignal ID/external ID
    - add user state observer

--- src/events/events.ts
+++ src/events/events.ts
@@ -8,3 +8,4 @@ export const IN_APP_MESSAGE_WILL_DISMISS = 'OneSignal-inAppMessageWillDismiss';
 export const IN_APP_MESSAGE_DID_DISMISS = 'OneSignal-inAppMessageDidDismiss';
 export const PERMISSION_CHANGED = 'OneSignal-permissionChanged';
 export const SUBSCRIPTION_CHANGED = 'OneSignal-subscriptionChanged';
+export const USER_STATE_CHANGED = 'OneSignal-userStateChanged';
