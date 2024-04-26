3b53d9e781cf5b207c520ca58d0b72ad278474ed
Jenna Antilla <46546946+jennantilla@users.noreply.github.com>
2024-04-26 13:56:01 -0700

    Add example app components
    - Add back existing example app components:
      - logging console
      - method buttons
      - helpers
    - Adjust styling of buttons for cross-OS uniformity & to conform with brand colors

--- examples/RNOneSignal/OSDemo.tsx
+++ examples/RNOneSignal/OSDemo.tsx
@@ -1,6 +1,9 @@
 import {LogLevel, OneSignal} from 'react-native-onesignal';
 import * as React from 'react';
 import {Alert, StyleSheet, View, ScrollView, SafeAreaView} from 'react-native';
+import OSButtons from './OSButtons';
+import OSConsole from './OSConsole';
+import {renderButtonView} from './Helpers';
 import {TextInput, Text} from '@react-native-material/core';
 
 const APP_ID = '77e32082-ea27-42e3-a898-c72e141824ef';
@@ -30,14 +33,122 @@ class OSDemo extends React.Component<Props, State> {
     OneSignal.initialize(APP_ID);
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
+    OneSignal.Notifications.addEventListener(
+      'foregroundWillDisplay',
+      (event) => {
+        this.OSLog('OneSignal: notification will show in foreground:', event);
+        let notif = event.getNotification();
+
+        const cancelButton = {
+          text: 'Cancel',
+          onPress: () => {
+            event.preventDefault();
+          },
+          style: 'cancel',
+        };
+
+        const completeButton = {
+          text: 'Display',
+          onPress: () => {
+            event.getNotification().display();
+          },
+        };
+
+        Alert.alert(
+          'Display notification?',
+          notif.title,
+          [cancelButton, completeButton],
+          {
+            cancelable: true,
+          },
+        );
+      },
+    );
+
+    OneSignal.Notifications.addEventListener('click', (event) => {
+      this.OSLog('OneSignal: notification clicked:', event);
+    });
+
+    OneSignal.InAppMessages.addEventListener('click', (event) => {
+      this.OSLog('OneSignal IAM clicked:', event);
+    });
+
+    OneSignal.InAppMessages.addEventListener('willDisplay', (event) => {
+      this.OSLog('OneSignal: will display IAM: ', event);
+    });
+
+    OneSignal.InAppMessages.addEventListener('didDisplay', (event) => {
+      this.OSLog('OneSignal: did display IAM: ', event);
+    });
+
+    OneSignal.InAppMessages.addEventListener('willDismiss', (event) => {
+      this.OSLog('OneSignal: will dismiss IAM: ', event);
+    });
+
+    OneSignal.InAppMessages.addEventListener('didDismiss', (event) => {
+      this.OSLog('OneSignal: did dismiss IAM: ', event);
+    });
+
+    OneSignal.User.pushSubscription.addEventListener(
+      'change',
+      (subscription) => {
+        this.OSLog('OneSignal: subscription changed:', subscription);
+      },
+    );
+
+    OneSignal.Notifications.addEventListener('permissionChange', (granted) => {
+      this.OSLog('OneSignal: permission changed:', granted);
+    });
+
+    OneSignal.User.addEventListener('change', (event) => {
+      this.OSLog('OneSignal: user changed: ', event);
+    });
   }
+
+  OSLog = (message: string, optionalArg: any = null) => {
+    if (optionalArg !== null) {
+      message = message + JSON.stringify(optionalArg);
+    }
+
+    console.log(message);
+
+    let consoleValue;
+
+    if (this.state.consoleValue) {
+      consoleValue = `${this.state.consoleValue}\n${message}`;
+    } else {
+      consoleValue = message;
+    }
+    this.setState({consoleValue});
+  };
+
+  inputChange = (text: string) => {
+    this.setState({inputValue: text});
+  };
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.title}>OneSignal</Text>
+          <OSConsole value={this.state.consoleValue} />
+          <View style={styles.clearButton}>
+            {renderButtonView('X', () => {
+              this.setState({consoleValue: ''});
+            })}
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="Input"
+            onChangeText={this.inputChange}
+          />
         </View>
+        <ScrollView style={styles.scrollView}>
+          <OSButtons
+            loggingFunction={this.OSLog}
+            inputFieldValue={this.state.inputValue}
+          />
+        </ScrollView>
       </SafeAreaView>
     );
   }
