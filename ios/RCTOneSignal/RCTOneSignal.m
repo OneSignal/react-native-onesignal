b882104185cd590272598577eaa959c44cdc60aa
Nan <nan@onesignal.com>
2025-07-01 13:37:57 -0700

    release commit 5.2.13

--- ios/RCTOneSignal/RCTOneSignal.m
+++ ios/RCTOneSignal/RCTOneSignal.m
@@ -47,7 +47,7 @@ OSNotificationClickResult* coldStartOSNotificationClickResult;
         return;
 
     OneSignalWrapper.sdkType = @"reactnative";
-    OneSignalWrapper.sdkVersion = @"050212"; 
+    OneSignalWrapper.sdkVersion = @"050213"; 
     // initialize the SDK with a nil app ID so cold start click listeners can be triggered
     [OneSignal initialize:nil withLaunchOptions:launchOptions];
     didInitialize = true;
