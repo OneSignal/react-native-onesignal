1f278a55b65b6a2b84ea6e4470d5d2b36df7ebf6
Fadi George <fadii925@gmail.com>
2025-12-09 14:51:19 -0800

    Add OneSignal to the NSE target

--- examples/RNOneSignalTS/ios/OneSignalNotificationServiceExtension/NotificationService.swift
+++ examples/RNOneSignalTS/ios/OneSignalNotificationServiceExtension/NotificationService.swift
@@ -1,35 +1,32 @@
-//
-//  NotificationService.swift
-//  OneSignalNotificationServiceExtension
-//
-//  Created by Fadi George on 12/9/25.
-//
-
 import UserNotifications
+import OneSignalExtension
 
 class NotificationService: UNNotificationServiceExtension {
-
     var contentHandler: ((UNNotificationContent) -> Void)?
+    var receivedRequest: UNNotificationRequest!
     var bestAttemptContent: UNMutableNotificationContent?
 
+    // Note this extension only runs when `mutable_content` is set
+    // Setting an attachment or action buttons automatically sets the property to true
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.receivedRequest = request
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
+        self.bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
         if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
-    
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
+            // DEBUGGING: Uncomment the 2 lines below to check this extension is executing
+//            print("Running NotificationServiceExtension")
+//            bestAttemptContent.body = "[Modified] " + bestAttemptContent.body
+
+            OneSignalExtension.didReceiveNotificationExtensionRequest(self.receivedRequest, with: bestAttemptContent, withContentHandler: self.contentHandler)
         }
     }
 
+    override func serviceExtensionTimeWillExpire() {
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            OneSignalExtension.serviceExtensionTimeWillExpireRequest(self.receivedRequest, with: self.bestAttemptContent)
+            contentHandler(bestAttemptContent)
+        }
+    }
 }
