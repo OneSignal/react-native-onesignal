d961e5bc4267f6df34f8df48a5c339b3f1d6139e
Fadi George <fadii925@gmail.com>
2025-12-15 12:33:14 -0800

    add fix for transforming array data

--- test.sh
+++ test.sh
@@ -5,6 +5,110 @@ set -a
 [ -f .env ] && . .env
 set +a
 
+# Test data
+read -r -d '' TEST_DATA_1 << 'EOF'
+{
+  "route": "Search",
+  "basket": [
+    {
+      "sku": "12qw3er56y",
+      "itemName": "Hand-crafted Peppermint Candy Canes",
+      "qty": 3,
+      "cost": "9.99"
+    },
+    {
+      "sku": "90ujr5383",
+      "itemName": "Fat Squirrel",
+      "qty": 1,
+      "cost": "29.99"
+    }
+  ]
+}
+EOF
+
+read -r -d '' TEST_DATA_2 << 'EOF'
+{
+  "route": "ComplexTest",
+  "stringValue": "test string",
+  "booleanValue": true,
+  "integerValue": 42,
+  "doubleValue": 3.14159,
+  "nullValue": null,
+  "stringArray": ["apple", "banana", "cherry"],
+  "numberArray": [1, 2, 3, 4, 5],
+  "booleanArray": [true, false, true],
+  "mixedArray": [
+    "string item",
+    123,
+    45.67,
+    true,
+    false,
+    null,
+    {
+      "nestedString": "value",
+      "nestedNumber": 999,
+      "nestedBoolean": false
+    },
+    [
+      "nested array item 1",
+      "nested array item 2",
+      789
+    ]
+  ],
+  "nestedObjects": {
+    "level1": {
+      "level2": {
+        "level3": {
+          "deepValue": "deeply nested",
+          "deepNumber": 1000,
+          "deepArray": [1, 2, 3]
+        }
+      }
+    }
+  },
+  "arrayOfObjects": [
+    {
+      "id": 1,
+      "name": "Item One",
+      "active": true,
+      "price": 19.99,
+      "tags": ["tag1", "tag2"]
+    },
+    {
+      "id": 2,
+      "name": "Item Two",
+      "active": false,
+      "price": 29.99,
+      "tags": ["tag3"],
+      "metadata": {
+        "created": "2024-01-01",
+        "updated": null
+      }
+    },
+    {
+      "id": 3,
+      "name": null,
+      "active": true,
+      "price": 0,
+      "tags": []
+    }
+  ],
+  "nestedArrays": [
+    [1, 2, 3],
+    ["a", "b", "c"],
+    [true, false],
+    [
+      {
+        "item": "nested in array in array",
+        "value": 42
+      }
+    ]
+  ],
+  "emptyArray": [],
+  "emptyObject": {}
+}
+EOF
+
 curl --request POST \
   --url 'https://api.onesignal.com/notifications?c=push' \
   --header "Authorization: Key $API_KEY" \
@@ -27,21 +131,5 @@ curl --request POST \
     \"en\": \"Test Subtitle\"
   },
   \"name\": \"abc123\",
-  \"data\": {
-    \"route\": \"Search\",
-    \"basket\": [
-      {
-        \"sku\": \"12qw3er56y\",
-        \"itemName\": \"Hand-crafted Peppermint Candy Canes\",
-        \"qty\": 3,
-        \"cost\": \"9.99\"
-      },
-      {
-        \"sku\": \"90ujr5383\",
-        \"itemName\": \"Fat Squirrel\",
-        \"qty\": 1,
-        \"cost\": \"29.99\"
-      }
-    ]
-  }
+  \"data\": $TEST_DATA_2
 }"
