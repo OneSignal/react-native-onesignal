9f423660cb4d32e7a7502eb3e6da902bc77b1bb7
Fadi George <fadii925@gmail.com>
2026-02-17 23:48:47 -0800

    delete sherwin demo

--- examples/demo-sherwin/constants/IamTemplates.ts
+++
@@ -1,6 +0,0 @@
-export const IamTypes = [
-  { id: 'top_banner', title: 'Top Banner', icon: '↑' },
-  { id: 'bottom_banner', title: 'Bottom Banner', icon: '↓' },
-  { id: 'center_modal', title: 'Center Modal', icon: '□' },
-  { id: 'fullscreen', title: 'Full Screen', icon: '⊞' },
-];
