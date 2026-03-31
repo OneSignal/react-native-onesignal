7ba81661ced881502f6aaf7513cfb11bd4ccff1b
Fadi George <fadii925@gmail.com>
2026-03-31 14:46:04 -0700

    refactor(demo): remove env dotenv config

--- examples/demo/babel.config.js
+++ examples/demo/babel.config.js
@@ -12,16 +12,4 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: '.env',
-        safe: false,
-        allowUndefined: true,
-      },
-    ],
-  ],
 };
